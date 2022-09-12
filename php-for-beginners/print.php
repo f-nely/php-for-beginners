3ca0bb30deb7c4537e16d57408a105fd373ea9aa
nely de faria <nelly.nf54@gmail.com>
2022-09-12 15:15:26 -0300

    Creating print.php

---
+++ php-for-beginners/print.php
@@ -0,0 +1,15 @@
+<?php
+
+// print
+print 'hello from print';
+
+print '<br>';
+
+// Differences between echo and print
+$var = print 'hello from var<br>';
+
+// $var_2 = echo 'hello from var_2'; error
+
+echo 'hello this arg one ', 'hello from 2';
+
+// print 'hello this arg one ', 'hello from 2';
