a7c91ca20335b3d662d35998eb38972ca7a2e128
nely de faria <nelly.nf54@gmail.com>
2022-09-12 16:20:47 -0300

    Creating increment-and-decrement-operators.php

---
+++ php-for-beginners/increment-and-decrement-operators.php
@@ -0,0 +1,20 @@
+<?php
+
+// increment-and-decrement operators
+
+/*
+    ++$x
+    $x++
+    --$x
+    $x--
+*/
+
+$num = 10;
+
+// echo ++$num;
+// $num++;
+// echo $num;
+
+// echo --$num;
+$num--;
+echo $num;
