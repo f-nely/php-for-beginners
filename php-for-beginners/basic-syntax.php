cca8decd6ecdbc4b7d843108707ba565ba5396fc
nely de faria <nelly.nf54@gmail.com>
2022-09-12 15:14:37 -0300

    Creating basic-syntax.php

---
+++ php-for-beginners/basic-syntax.php
@@ -0,0 +1,18 @@
+<?php
+
+echo 'PHP rocks';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= 'PHP Course' ?></title>
+</head>
+<body>
+    <p><?= 'this is our paragraph' ?></p>
+</body>
+</html>
