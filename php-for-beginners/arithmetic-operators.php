65340813f2711249fbb6e2cddfce805d01876a2f
nely de faria <nelly.nf54@gmail.com>
2022-09-12 16:19:23 -0300

    Creating arithmetic-operators.php

---
+++ php-for-beginners/arithmetic-operators.php
@@ -0,0 +1,24 @@
+<?php
+
+// arithmetic operators
+/*
+    + : Addition 
+    - : Subtraction
+    * : Multiplication
+    / : Division
+    % : Modulo
+    ** : Exponentiation
+*/
+// echo 8 + 8;
+
+// echo 8 + 8.9;
+
+// echo 12 - 11;
+
+// echo 22 * 1;
+
+// echo 11 / 3;
+
+// echo 9 % 3;
+
+echo 5 ** 3;
