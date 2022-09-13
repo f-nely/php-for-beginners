804447a5adb67d20569f0d6a96cda8ef2437ee3b
nely de faria <nelly.nf54@gmail.com>
2022-09-13 16:36:57 -0300

    Creating switch.php

---
+++ php-for-beginners/switch.php
@@ -0,0 +1,17 @@
+<?php
+
+$favoriteColor = 'blue';
+
+switch ($favoriteColor) {
+    case 'red':
+        echo 'your favorite color is red';
+        break;
+    case 'green':
+        echo 'your favorite color is green';
+        break;
+    case 'blue':
+        echo 'your favorite color is blue';
+        break;
+    default:
+        echo 'you like another color';
+}
