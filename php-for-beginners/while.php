52e998cdfda177221a4918511db08bc61e009c46
nely de faria <nelly.nf54@gmail.com>
2022-09-13 16:37:16 -0300

    Creating while.php

---
+++ php-for-beginners/while.php
@@ -0,0 +1,19 @@
+<?php
+
+// while and the for loops
+// the intilization
+
+$j = 0;
+while ($j <= 10) {
+    // the increment or the decrement
+    echo $j++;
+    echo '<br>';
+}
+
+echo "----------------------------<br>";
+
+$j = 20;
+while ($j >= 0) {
+    echo $j--;
+    echo '<br>';
+}
