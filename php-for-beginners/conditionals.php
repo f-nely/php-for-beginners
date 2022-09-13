1f35d213fbfaa1350617d3c63ba54c628e035eba
nely de faria <nelly.nf54@gmail.com>
2022-09-13 16:36:14 -0300

    Creating conditionals.php

---
+++ php-for-beginners/conditionals.php
@@ -0,0 +1,11 @@
+<?php
+
+$age = 18;
+
+if ($age == 12) {
+    echo 'you should not be here';
+} else if ($age == 18) {
+    echo 'your 18 your perfectly welcome';
+} else {
+    echo 'you are welcome here';
+}
