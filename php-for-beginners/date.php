79ef81c17c930bae5287f1fb6632518e300a0513
nely de faria <nelly.nf54@gmail.com>
2022-09-15 15:03:36 -0300

    Creating date.php

---
+++ php-for-beginners/date.php
@@ -0,0 +1,10 @@
+<?php
+
+// >> d - Represents the day of the month (01 to 31)
+// >> m - Represents a month (01 to 12)
+// >> Y - Represents a year (in four digits)
+// >> l (lowercase 'L') - Represents the day of the week
+
+echo date('d/m/Y');
+echo '<br>';
+echo "What's day is it today? It's " . date('l');
