24a96c9ddcbc779f7764b1297eca50dde65a3bf2
nely de faria <nelly.nf54@gmail.com>
2022-09-17 12:47:59 -0300

    Set charset

--- php-for-beginners/conn.php
+++ php-for-beginners/conn.php
@@ -7,7 +7,7 @@ $user = 'root';
 $password = '<root-password>';
 
 try {
-    $conn = new PDO("mysql:host={$host};dbname={$dbname}", $user, $password);
+    $conn = new PDO("mysql:host={$host};dbname={$dbname}; charset=utf8", $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch(PDOException $e) {
